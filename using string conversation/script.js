72797084ce3eb2b1fe085ea54f67124e01752e8f
coadwithsohel <160348170+coadwithsohel@users.noreply.github.com>
2024-02-24 17:48:15 +0530

    Add files via upload

---
+++ using string conversation/script.js
@@ -0,0 +1,41 @@
+// let value ="56"
+// let negativeValue = -value
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// let str1 ="hello"
+// let str2 = "sohel"
+// let str3 = str1+str2
+// console.log(str3);
+// console.log(56 + 90);
+
+// console.log(+true);      /* is is denoted  by 1 means true value*/
+// console.log(+"");        /*it is  denoted by 0 means false value*/
+
+
+let num1 , num2,num3;
+
+num1 = num2 = num3 = 2 + 2 
+
+
+let gameCounter = 10 ;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
