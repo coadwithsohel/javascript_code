f217a5fea3ba315fd81fef9475f311d6bc0ae703
coadwithsohel <160348170+coadwithsohel@users.noreply.github.com>
2024-02-26 17:52:12 +0530

    Add files via upload

---
+++ string  and boolean/script.js
@@ -0,0 +1,14 @@
+let isLoggedeIn = "sohel";
+let booleanIsLoggedIn= Boolean(isLoggedeIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 = true it should want a any name or value
+// 0 = false ,meanse no element here the value is false 
+// "" = empty string are used then output will be false
+
+
+let someNumber = "456"
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
